dddca795de837ccec44edc50fd2938c56a4f60ba
Septian <you@example.com>
2021-07-19 13:26:23 +0700

    add search and pagination

--- src/components/TableCompnt.js
+++ src/components/TableCompnt.js
@@ -1,53 +1,86 @@
-import React from 'react'
-import BootstrapTable from 'react-bootstrap-table-next';
-import { Container, Button } from 'reactstrap';
-import { faEdit, faInfo, faTrash } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from "react";
+import BootstrapTable from "react-bootstrap-table-next";
+import { Container, Button } from "reactstrap";
+import { faEdit, faInfo, faTrash } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import ToolkitProvider, { Search } from "react-bootstrap-table2-toolkit";
+import paginationFactory from 'react-bootstrap-table2-paginator';
 
-const columns = [{
-    dataField: 'id',
-    text: 'ID',
-    headerStyle:() => {
-        return { width: "5%"};
+
+const { SearchBar } = Search;
+
+const columns = [
+  {
+    dataField: "id",
+    text: "ID",
+    sort: true,
+    headerStyle: () => {
+      return { width: "7%" };
     },
-}, {
-    dataField: 'nama',
-    text: 'Name'
-}, {
-    dataField: 'alamat',
-    text: 'Alamat'
-}, {
-    dataField: 'link',
-    text: 'Aksi',
+  },
+  {
+    dataField: "nama",
+    text: "Name",
+    sort: true,
+  },
+  {
+    dataField: "alamat",
+    text: "Alamat",
+    sort: true,
+  },
+  {
+    dataField: "link",
+    text: "Aksi",
     formatter: (rowContent, row) => {
-        return(
-            <div>
-                <Button color="dark" className="mr-2">
-                    <FontAwesomeIcon icon={faInfo}/>
-                </Button>
+      return (
+        <div>
+          <Button color="dark" className="mr-2">
+            <FontAwesomeIcon icon={faInfo} />
+          </Button>
 
-                <Button color="dark" className="mr-2">
-                    <FontAwesomeIcon icon={faEdit}/>
-                </Button>
-                
-                <Button color="dark" className="mr-2">
-                    <FontAwesomeIcon icon={faTrash}/>
-                </Button>
-            </div>
-        )
-    }
-}];
+          <Button color="dark" className="mr-2">
+            <FontAwesomeIcon icon={faEdit} />
+          </Button>
 
+          <Button color="dark" className="mr-2">
+            <FontAwesomeIcon icon={faTrash} />
+          </Button>
+        </div>
+      );
+    },
+  },
+];
 
+const defaultSorted = [
+  {
+    dataField: "id",
+    order: "asc",
+  },
+];
 
 const TableCompnt = (props) => {
-    return (
-        <Container>
-            
-            <BootstrapTable keyField='id' data={props.users} columns={columns}/>
-        </Container>
-        
-    )
-}
+  return (
+    <Container>
+      <ToolkitProvider
+        bootstrap4
+        keyField="id"
+        data={props.users}
+        columns={columns}
+        defaultSorted={defaultSorted}
+        search
+      >
+        {(props) => (
+          <div>
+              <div className="float-right">
+                <SearchBar {...props.searchProps} placeholder="Search.."/>
+              </div>
+            <hr />
+            <BootstrapTable {...props.baseProps} pagination={ paginationFactory() }/>
+          </div>
+        )}
+      </ToolkitProvider>
+    </Container>
+  );
+};
 
-export default TableCompnt
+export default TableCompnt;
